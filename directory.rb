c17bf910fcab364dd1e84552c4f7da0492284d74
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-14 11:32:19 +0000

    interactive method to generate hash of name and house created

--- directory.rb
+++ directory.rb
@@ -1,27 +1,21 @@
-wizard_count = 6
-#Let's put all students into an array
-wizards = [ {name: "Neville Longbottom", house: "Gryffindor"},
-  {name: "Draco Malfoy", house: "Slytherin"},
-  {name: "Harry Potter", house: "Gryffindor"},
-  {name: "Ron Weasley", house: "Gryffindor"},
-  {name: "Alicia Spinnet", house: "Gryffindor"},
-  {name: "Oliver Wood", house: "Gryffindor"},
-  {name: "Cedric Diggory", house: "Hufflepuff"}
+def input_wizards
+  puts "Please enter the names of the witches and wizards"
+  puts "To finish, just type stop"
+  #create an empty array
+  wizards = []
+  #get the name of the wizard
+  name = gets.chomp
+  #while the name is not equal to stop, repeat this code
+  while name != "stop" do
+    #add the wizard hash to the array
+    wizards << {name: name, house: :Gryffindor}
+    puts "Now we have #{wizards.length} wizards and witches"
+    # get another name from the user
+    name = gets.chomp
+  end
+  #return the array of wizards
+  wizards
 
-]
-def print_header
-  puts "The students of Hogwarts School of Witchcraft and Wizardry"
-  puts "----------------------------------------------------------"
-end
-def print(wizards)
-  wizards.each do |wizard|
-  puts "#{wizard[:name]} (#{wizard[:house]} house)"
-end
-end
-def print_footer(wizards)
-  puts "Overall we have #{wizards.length} incredible wizards (some of them are also fantastic quidditch players)"
 end
 
-print_header
-print(wizards)
-print_footer(wizards)
+puts input_wizards
