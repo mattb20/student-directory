e0260172d21ce26b8309ea793f2cca1a2b1f3563
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-14 08:33:51 +0000

    added some comments

--- directory.rb
+++ directory.rb
@@ -1,12 +1,13 @@
 puts "The students of Hogwarts School of Witchcraft and Wizardry"
 puts "----------------------------------------------------------"
+# First we print the witches and wizards
 puts "Neville Longbottom"
 puts "Draco Malfoy"
 puts "Harry Potter"
 puts "Ron Weasley"
 puts "Alicia Spinnet"
 puts "Oliver Wood"
-
+#Then we give a total
 print "Overall, we have "
 print 6
 puts " incredible wizards (some of them are also fantastic quidditch players)"
