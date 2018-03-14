ec565f8a33a2d3bf4b198a45b2a9748981ba3e2e
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-14 09:24:48 +0000

    printing the students from an array in a very repeated code

--- directory.rb
+++ directory.rb
@@ -1,12 +1,22 @@
-student_count = 6
+wizard_count = 6
+# First we print the witches and wizards
+#Let's put all students into an array
+wizards = ["Neville Longbottom",
+  "Draco Malfoy",
+  "Harry Potter",
+  "Ron Weasley",
+  "Alicia Spinnet",
+  "Oliver Wood"
+
+]
+#and then print them(a horrible way!!)
 puts "The students of Hogwarts School of Witchcraft and Wizardry"
 puts "----------------------------------------------------------"
-# First we print the witches and wizards
-puts "Neville Longbottom"
-puts "Draco Malfoy"
-puts "Harry Potter"
-puts "Ron Weasley"
-puts "Alicia Spinnet"
-puts "Oliver Wood"
+puts wizards[0]
+puts wizards[1]
+puts wizards[2]
+puts wizards[3]
+puts wizards[4]
+puts wizards[5]
 #Then we give a total
-puts "Overall we have #{student_count} incredible wizards (some of them are also fantastic quidditch players)"
+puts "Overall we have #{wizard_count} incredible wizards (some of them are also fantastic quidditch players)"
