d4fa6112108cdaec5187b3d09188adeeb48c932b
mattb20 <34608245+mattb20@users.noreply.github.com>
2018-03-15 16:28:38 +0000

    interactive menu with 3 different options for user

--- directory.rb
+++ directory.rb
@@ -1,3 +1,32 @@
+def interactive_menu
+  wizards = []
+  loop do
+    #print the menu and ask the user what to do
+    puts "1. Input the wizards"
+    puts "2. Show the wizards"
+    puts "3. Show wizards/witches in a certain house"
+    puts "9. Exit"
+    #read the input and save it into a variable
+    selection = gets.chomp
+    #do what the user has asked
+    case selection
+      when "1"
+      # input the wizards
+        wizards = input_wizards
+      when "2"
+      # show the wizards
+        print_header
+        print(wizards)
+        print_footer(wizards)
+      when "3"
+       print_by_house(wizards)
+      when "9"
+        exit # will cause program to terminate
+      else
+        puts "I don't know what you meant try again"
+      end
+    end
+  end
 def input_wizards
   puts "Please enter the names of the witches and wizards"
   puts "To finish, just type stop"
@@ -34,7 +63,15 @@ def print_footer(wizards)
   puts "Overall, we have #{wizards.length} great wizards and witches, some being fantastic quidditch players"
 end
 
-wizards = input_wizards
-print_header
-print(wizards)
-print_footer(wizards)
+def print_by_house(wizards)
+  puts "Please enter the name of the house you would like to print"
+  house = gets.chomp.downcase
+  wizards.each do |wizard|
+      if wizard[:house].downcase == house
+        puts ("We have #{wizard[:name]} of #{wizard[:house]}").center(58)
+      end
+    end
+end
+
+
+interactive_menu
